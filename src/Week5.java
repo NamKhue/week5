634a556fbc183603bb1de687bf2942642762a631
cootanasy <namkhue2606@gmail.com>
2023-10-04 22:42:31 +0700

    week 5

--- src/Week5.java
+++ src/Week5.java
@@ -1,16 +1,18 @@
 public class Week5 {
-    public static String PriceOfTicket(int a) {
+    public static String PriceOfTicket(int x) {
         String result = "";
-        if (a < 50 || a > 300) {
-            result = "Chiều cao không hợp lệ. Xin nhập lại";
-        } else if (a < 100) {
+
+        if (x < 100) {
             result = "Miễn Phí";
-        } else if (a < 120) {
+        } else if (x < 120) {
             result = "30000 đồng";
-        } else if (a < 140) {
+        } else if (x < 140) {
             result = "60000 đồng";
-        } else if (a < 300) {
+        } else if (x < 300) {
             result = "80000 đồng";
+        } else {
+            // x < 50 || x > 300
+            result = "Chiều cao không hợp lệ. Xin nhập lại";
         }
         return result;
     }
